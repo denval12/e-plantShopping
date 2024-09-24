0c1dcd183642e7d284ec28146fcccbb0d99298d6
denval12 <deniserecuperacion12@gmail.com>
2024-09-23 20:12:40 -0400

    Update button Add to Cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -290,7 +290,7 @@ const handlePlantsClick = (e) => {
                                 <img className="product-image" src={plant.image} alt={plant.name} />
                                 <div className="product-title">{plant.name}</div>
                                 {/*Similarly like the above plant.name show other details like description and cost*/}
-                                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                <button  className={addedToCart[plant.name] ? 'product-button.added-to-cart' : 'product-button'} disabled={addedToCart[plant.name]}  onClick={() => handleAddToCart(plant)}>{addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}</button>
                             </div>
                             ))}
                         </div>
